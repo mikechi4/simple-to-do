e460235be63667a3dc8ecfef42b853b99bc0f254
Mike Chi <mike.chi4@gmail.com>
2019-07-16 21:27:49 -0400

    using prettier library to format code

--- src/components/ListContainer.js
+++ src/components/ListContainer.js
@@ -1,25 +1,22 @@
-import React from 'react';
+import React from "react";
 
-import ToDoItem from './ToDoItem';
+import ToDoItem from "./ToDoItem";
 
-const ListContainer = (props) => {
+const ListContainer = props => {
+  const renderList = () => {
+    return props.items.map(item => {
+      return (
+        <ToDoItem toDoItem={item} key={item.task} moveItem={props.moveItem} />
+      );
+    });
+  };
 
-    const renderList = () => {
-        console.log(props)
-        return props.items.map(item => {
-            return(
-                <ToDoItem toDoItem={item} key={item.task} moveItem={props.moveItem}/>
-            );
-        })
-    };
-
-    return(
-        <div className="to-do-section ui segment">
-            <h2>{props.header}</h2>
-            {renderList()}
-        </div>
-    )
-
-}
+  return (
+    <div className="to-do-section ui segment">
+      <h2>{props.header}</h2>
+      {renderList()}
+    </div>
+  );
+};
 
 export default ListContainer;
--- src/components/ToDoInput.js
+++ src/components/ToDoInput.js
@@ -1,38 +1,39 @@
-import React from 'react';
+import React from "react";
 
 class ToDoInput extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: ''
-        };
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: ""
     };
+  }
 
-    setToDo = (event) => {
-        this.setState({value: event.target.value});
-    }
+  setToDo = event => {
+    this.setState({ value: event.target.value });
+  };
 
-    onSubmit = (event) => {
-        event.preventDefault();
-        this.props.addToDo({task: this.state.value, status: 'TO_DO'});
-    }
-    render() {
-        return(
-            <div className='to-do-input'>
-                <form  onSubmit = {this.onSubmit} style={{textAlign: 'center'}}>
-                    <label>
-                        What do you need done? <br />
-                        <input 
-                            type="text" 
-                            value={this.state.value} 
-                            onChange={this.setToDo}
-                        />
-                    </label>
-                    <button className='positive ui button'>Add</button>
-                </form>
-            </div>
-        )
-    }
+  onSubmit = event => {
+    event.preventDefault();
+    this.props.addToDo({ task: this.state.value, status: "TO_DO" });
+    this.setState({ value: "" });
+  };
+  render() {
+    return (
+      <div className="to-do-input">
+        <form onSubmit={this.onSubmit} style={{ textAlign: "center" }}>
+          <label>
+            What do you need done? <br />
+            <input
+              type="text"
+              value={this.state.value}
+              onChange={this.setToDo}
+            />
+          </label>
+          <button className="positive ui button">Add</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default ToDoInput;
