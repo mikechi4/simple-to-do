aba1ae4652e56524d1eaf6b60f3c0f5bbce5e498
Mike Chi <mike.chi4@gmail.com>
2019-07-16 21:25:33 -0400

    instead of clicking to move to do item to next field, we will now incorporate a menu to allow the user to choose where they would like to move the menu

--- src/components/ToDoItem.js
+++ src/components/ToDoItem.js
@@ -1,13 +1,59 @@
-import React from 'react';
+import React from "react";
 
-import './ToDoItem.css';
-const ToDoItem = (props) => {
+import "./ToDoItem.css";
+class ToDoItem extends React.Component {
+  constructor() {
+    super();
 
+    this.state = {
+      showMenu: false
+    };
+  }
+
+  showMenu = event => {
+    event.preventDefault();
+
+    this.setState({
+      showMenu: (this.state.showMenu = !this.state.showMenu)
+    });
+  };
+
+  render() {
     return (
-        <div className='ui segment raised to-do-item' onClick={() => props.moveItem(props.toDoItem)}>
-            {props.toDoItem.task}
-        </div>
-    )
+      <div className="ui segment raised to-do-item">
+        {this.props.toDoItem.task}
+        <i class="fa fa-ellipsis-v to-do-item__menu" onClick={this.showMenu} />
+        {this.state.showMenu ? (
+          <div className="ui segment raised to-do-item__popup">
+            <div className="to-do-item__popup__header">
+              Move to:
+              <button
+                onClick={() =>
+                  this.props.moveItem(this.props.toDoItem, "TO_DO")
+                }
+              >
+                To Do
+              </button>
+              <button
+                onClick={() =>
+                  this.props.moveItem(this.props.toDoItem, "IN_PROGRESS")
+                }
+              >
+                In Progress
+              </button>
+              <button
+                onClick={() =>
+                  this.props.moveItem(this.props.toDoItem, "COMPLETED")
+                }
+              >
+                Completed
+              </button>
+            </div>
+          </div>
+        ) : null}
+      </div>
+    );
+  }
 }
 
 export default ToDoItem;
